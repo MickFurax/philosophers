5039be2decefc529cc042dfdb8c3f205fe4e5772
MickFurax <mickfurax@gmail.com>
2024-05-20 15:31:26 +0300

    add header file

---
+++ philosophers.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabeman <arabeman@student.42antanana      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 10:59:58 by arabeman          #+#    #+#             */
+/*   Updated: 2024/05/20 11:00:01 by arabeman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+typedef struct s_philospher
+{
+	pthread_t	thread;
+	int			number;
+}				t_philosopher;
+
+#endif
